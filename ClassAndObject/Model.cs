4da940d288f41f6a49472ac410aeaa5b09cfba6d
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-29 22:31:41 +0500

    fix mistake

--- ClassAndObject/Model.cs
+++ ClassAndObject/Model.cs
@@ -8,32 +8,42 @@ namespace ClassAndObject
 {
     public class Model: IModel
     {
+        
+
         private List<Time> timeArray;
         private int indexArray;
 
         public int index { set { if (indexArray < timeArray.Count) indexArray = value; } }
 
-        public Time element { get { return timeArray[indexArray]; } }
+        public int count { get { return timeArray.Count(); } }
+
+        public Time element { get { return timeArray[indexArray]; } set { timeArray[indexArray] = value;} }
 
         public List<Time> allArray { get { return timeArray; } }
 
-        public Model() { }
+        public Model() { timeArray = new List<Time>(); }
 
         public Model(int size)
         {
             Random rnd = new Random();
+            timeArray = new List<Time>();
 
             for (int i = 0; i < size; i++)
-                timeArray.Add(new Time(rnd.Next(23), rnd.Next(59)));
+                timeArray.Add(new Time(rnd.Next(22), rnd.Next(58)));
+            
         }
 
         public Model (params Time [] timeArr)
         {
+            timeArray = new List<Time>();
             for (int i = 0; i < timeArr.Count(); i++)
+            {
                 timeArray.Add(timeArr[i]);
+              
+            }
         }
 
-        void AddElement(Time time)
+        public void AddElement(Time time)
         {
             timeArray.Add(time);
         }
